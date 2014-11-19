b109d53534419a97054bdd46c3f3e8879e3c3687
Shawn Krisman <krisman.shawn@gmail.com>
2014-11-19 00:41:06 -0800

    have a fight scene.

--- js/game.js
+++ js/game.js
@@ -1,14 +1,25 @@
 var startGame = function() {
     Crafty.init(1333, 800, "crafty-star-anise");
-    Crafty.e('StartScreen');
+    Crafty.scene('StartScene');
 };
+
+Crafty.scene('StartScene', function() {
+    Crafty.e('StartScreen');
+});
+
+Crafty.scene('FightScene', function() {
+    Crafty.background('white');
+});
 
 Crafty.c('StartScreen', {
     init: function() {
-        this.requires('2D, Canvas, SpriteAnimation, BackgroundSprite');
+        this.requires('2D, Canvas, SpriteAnimation, BackgroundSprite, Mouse');
         this.attr({x: 0, y: 0});
         this.reel('BackgroundAnimating', 1000, 0, 0, 6);
         this.animate('BackgroundAnimating', -1);
+        this.bind('MouseUp', function() {
+            Crafty.scene('FightScene');
+        });
     }
 });
 
